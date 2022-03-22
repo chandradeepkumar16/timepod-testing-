f7e47df71e19f1ef3b48896454b346d7f4e254d2
_ayusshhhii_ <2as1931154@gmail.com>
2022-03-22 15:04:07 +0530

    splash screen

---
+++ app/src/main/java/com/example/timepod/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.timepod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Handler h= new Handler();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
