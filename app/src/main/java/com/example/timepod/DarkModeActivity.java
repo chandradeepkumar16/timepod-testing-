aca64249567c0d9bd747034b603b529b65ee709a
_ayusshhhii_ <2as1931154@gmail.com>
2022-03-31 00:34:24 +0530

    added click listeners in profile activity

---
+++ app/src/main/java/com/example/timepod/DarkModeActivity.java
@@ -0,0 +1,14 @@
+package com.example.timepod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DarkModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dark_mode);
+    }
+}
--- app/src/main/java/com/example/timepod/ProfileActivity.java
+++ app/src/main/java/com/example/timepod/ProfileActivity.java
@@ -19,6 +19,12 @@ import butterknife.OnClick;
 public class ProfileActivity extends AppCompatActivity {
 
     TextView settings;
+    TextView timer;
+    TextView pomodoro;
+    TextView help;
+    TextView dark;
+    TextView notes;
+    TextView friend;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -37,6 +43,70 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        timer= (TextView)findViewById(R.id.setatimertext);
+        timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, TimerActivity.class);
+                startActivity(intent);
 
+                Toast.makeText(ProfileActivity.this, "clicked on set a timer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        pomodoro= (TextView)findViewById(R.id.pomodorotext);
+        pomodoro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, PomodoroActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on pomodoro", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        help= (TextView)findViewById(R.id.helptext);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, HelpActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on help", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dark= (TextView)findViewById(R.id.darkmodetext);
+        dark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, DarkModeActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on dark mode", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        notes= (TextView)findViewById(R.id.addnotestext);
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, AddNotesActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on add notes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        friend= (TextView)findViewById(R.id.tellyourfriendtext);
+        friend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, TellYourFriendActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on tell your friend", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
