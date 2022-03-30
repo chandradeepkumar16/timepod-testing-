34f60d841404c60967be2f66a939d3fff7f40645
_ayusshhhii_ <2as1931154@gmail.com>
2022-03-30 22:59:11 +0530

    Added profile activity

--- app/src/main/java/com/example/timepod/ProfileActivity.java
+++ app/src/main/java/com/example/timepod/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.example.timepod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        textView= (TextView)findViewById(R.id.settingstext);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ProfileActivity.this, SettingsActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(ProfileActivity.this, "clicked on settings", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
