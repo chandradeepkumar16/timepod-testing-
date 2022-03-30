79211e4628c6b0e2362483c7ba195951c2467489
_ayusshhhii_ <2as1931154@gmail.com>
2022-03-31 00:03:32 +0530

    Updated profile activity

--- app/src/main/java/com/example/timepod/ProfileActivity.java
+++ app/src/main/java/com/example/timepod/ProfileActivity.java
@@ -2,23 +2,32 @@ package com.example.timepod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.OnClick;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView settings;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        textView= (TextView)findViewById(R.id.settingstext);
-        textView.setOnClickListener(new View.OnClickListener() {
+        settings= (TextView)findViewById(R.id.settingstext);
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(ProfileActivity.this, SettingsActivity.class);
@@ -27,5 +36,7 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(ProfileActivity.this, "clicked on settings", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 }
